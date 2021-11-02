86522ebffaad60a2b707592ca301aaf5432893a1
99angelrm <99angelrm@gmail.com>
2021-11-02 17:30:56 -0600

    Validaciones hechas

--- controllers/contenido.js
+++ controllers/contenido.js
@@ -1,121 +1,154 @@
-const Contenido = require('../models/contenido')
+const Contenido = require("../models/contenido");
 const mongoose = require("mongoose");
-const { json } = require('express');
-const contenido = require('../models/contenido');
+const { json } = require("express");
+const contenido = require("../models/contenido");
 
-exports.postAgregarContenido = async (req,res)=>{
-    const contenido = new Contenido(req.body)
-    contenido.playlist = "none"
-    const DatoRepetido = await Contenido.findOne({ 
-            tipo : req.body.tipo,
-            titulo : req.body.titulo
+exports.postAgregarContenido = async (req, res) => {
+    const contenido = new Contenido(req.body);
+    contenido.playlist = "none";
+    const DatoRepetido = await Contenido.findOne({
+        tipo: req.body.tipo,
+        titulo: req.body.titulo,
     });
-    
+
     if (DatoRepetido === null) {
-        try{
+        try {
             //Agregar el documento a la colección
-            await contenido.save()
-            console.log(contenido)
-            console.log("Ha sido registrado")
-            res.status(200).send({operacion:"correcta"})
-        }catch(err){
-            console.log(err)
-            res.status(200).send({operacion: "incorrecta"})
+            await contenido.save();
+            console.log(contenido);
+            console.log("Ha sido registrado");
+            res.status(200).send({ operacion: "correcta" });
+        } catch (err) {
+            console.log(err);
+            res.status(200).send({ operacion: "incorrecta" });
         }
-    }else{
-        console.log("Ya está registrado")
-        res.status(422).json({estado:"ya existe"})
-        
+    } else {
+        console.log("Ya está registrado");
+        res.status(422).json({ estado: "ya existe" });
     }
-}
+};
 
-exports.getObtenerContenido=async (req,res)=>{
-    const cont = await Contenido.find() //modificar para que lo encuentre por pais
-    console.log("Obtener el contenido deseado")
-    console.log(cont)
-    res.status(200).json(cont)
-}
+exports.getObtenerContenido = async (req, res) => {
+    const cont = await Contenido.find(); //modificar para que lo encuentre por pais
+    console.log("Obtener el contenido deseado");
+    console.log(cont);
+    res.status(200).json(cont);
+};
 
-exports.postActualizarContenido = async(req,res)=>{
+exports.postActualizarContenido = async (req, res) => {
     //Filtro y cambio
-    try{
-        await Contenido.findOneAndUpdate({tipo:req.body.tipo,titulo:req.body.titulo},{año:req.body.año})
-        console.log("Cambio realizado")
-        res.status(200).json({operacion: "correcta"})
-    }catch(err){
-        console.log(err)
-        res.status(422).json({operacion: "incorrecta"})
+    try {
+        const dato = await Contenido.findOneAndUpdate(
+            { tipo: req.body.tipo, titulo: req.body.titulo },
+            { año: req.body.año }
+        );
+        if (dato === null) {
+            res.status(422).json({
+                estado: "Contenido inexistente",
+            });
+        } else {
+            console.log("Cambio realizado");
+            res.status(200).json({ operacion: "correcta" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(422).json({ operacion: "incorrecta" });
     }
-}
+};
 
-exports.postBorrarContenido = async (req,res)=>{
-    await Contenido.findOneAndDelete({tipo:req.body.tipo,titulo:req.body.titulo})
-    console.log("Ha sido eliminadx")
-    res.status(200).json({operacion: "correcta"})
-}
+exports.postBorrarContenido = async (req, res) => {
+    const data = await Contenido.findOneAndDelete({
+        tipo: req.body.tipo,
+        titulo: req.body.titulo,
+    });
+    if(data===null){
+        res.status(422).json({
+            estado: "Contenido inexistente",
+        });
+    }else{
+        console.log("Ha sido eliminadx");
+        res.status(200).json({ operacion: "correcta" });
+    }
+};
 
 //playlist
-exports.postAgregaraLaPlaylist = async (req,res)=>{
+exports.postAgregaraLaPlaylist = async (req, res) => {
     try {
-        const dato = await Contenido.findOneAndUpdate({tipo:req.body.tipo,titulo:req.body.titulo},{playlist:req.body.playlist})
-        console.log(dato)
-        if(dato===null){
+        const dato = await Contenido.findOneAndUpdate(
+            { tipo: req.body.tipo, titulo: req.body.titulo },
+            { playlist: req.body.playlist }
+        );
+        console.log(dato);
+        if (dato === null) {
             res.status(422).json({
-                "estado":"Contenido inexistente"
-            })
+                estado: "Contenido inexistente",
+            });
+        } else {
+            res.status(200).json({
+                estado: "correcto",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(422).json({
+            estado: "incorrecto",
+        });
+    }
+};
+
+exports.getPlaylist = async (req, res) => {
+    try {
+        const playlist = await Contenido.find({
+            playlist: req.params.playlist,
+        });
+        if(playlist.length()>0){
+            res.status(200).json(playlist);
         }
         else{
-            res.status(200).json({
-                "estado":"correcto"
-            })
+            res.status(422).json({
+                estado: "Contenido inexistente",
+            });
         }
-        
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status(422).json({
-            "estado":"incorrecto"
-        })
+            estado: "incorrecto",
+        });
     }
+};
 
-}
-
-exports.getPlaylist = async (req,res)=>{
+exports.postActualizarPlaylist = async (req, res) => {
     try {
-        const playlist = await Contenido.find({playlist:req.params.playlist})
-        res.status(200).json(playlist )
-        
-    } catch (error) {
-        console.log(error)
-        res.status(422).json({
-            "estado":"incorrecto"
-        })
+        const data = await Contenido.findOneAndUpdate(
+            { playlist: req.body.playlist, titulo: req.body.titulo },
+            { playlist: "none" }
+        );
+        if(data===null){
+            res.status(422).json({
+                estado: "Contenido inexistente",
+            });
+        }
+        else{
+            res.status(200).json({ operacion: "correcta" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(422).json({ operacion: "incorrecta" });
     }
+};
 
-}
-
-exports.postActualizarPlaylist = async (req,res)=>{
-    try{
-        await Contenido.findOneAndUpdate({"playlist":req.body.playlist,"titulo":req.body.titulo},{"playlist":"none"})
-        console.log("Cambio realizado")
-        res.status(200).json({operacion: "correcta"})
-    }catch(err){
-        console.log(err)
-        res.status(422).json({operacion: "incorrecta"})
-    }
-
-}
-
-exports.postBorrarPlaylist = async (req,res)=>{
+exports.postBorrarPlaylist = async (req, res) => {
     try {
-        await Contenido.updateMany({"playlist":req.body.playlist},{"$set":{"playlist":"none"}})
+        await Contenido.updateMany(
+            { playlist: req.body.playlist },
+            { $set: { playlist: "none" } }
+        );
         //console.log("Ha sido eliminadx")
         //await Contenido.remove()
-        res.status(200).json({operacion: "correcta"})
-        
+        res.status(200).json({ operacion: "correcta" });
     } catch (error) {
         res.status(422).json({
-            "estado":"incorrecto"
-        })
-        
+            estado: "incorrecto",
+        });
     }
-}
+};
