af7084d9f63f3c3b6cc4c06c6416255df00d3987
99angelrm <99angelrm@gmail.com>
2021-11-02 15:19:35 -0600

    Added status

--- controllers/contenido.js
+++ controllers/contenido.js
@@ -6,7 +6,6 @@ const contenido = require('../models/contenido');
 exports.postAgregarContenido = async (req,res)=>{
     const contenido = new Contenido(req.body)
     contenido.playlist = "none"
-    //CHECAR SI NO ESTA REPETIDO -- FALTA AUMENTAR CONTADOR 
     const DatoRepetido = await Contenido.findOne({ 
             tipo : req.body.tipo,
             titulo : req.body.titulo
@@ -18,14 +17,14 @@ exports.postAgregarContenido = async (req,res)=>{
             await contenido.save()
             console.log(contenido)
             console.log("Ha sido registrado")
-            res.send({operacion:"correcta"})
+            res.status(200).send({operacion:"correcta"})
         }catch(err){
             console.log(err)
-            res.send({operacion: "incorrecta"})
+            res.status(200).send({operacion: "incorrecta"})
         }
     }else{
         console.log("Ya está registrado")
-        res.json({estado:"ya existe"})
+        res.status(422).json({estado:"ya existe"})
         
     }
 }
@@ -34,7 +33,7 @@ exports.getObtenerContenido=async (req,res)=>{
     const cont = await Contenido.find() //modificar para que lo encuentre por pais
     console.log("Obtener el contenido deseado")
     console.log(cont)
-    res.json(cont)
+    res.status(200).json(cont)
 }
 
 exports.postActualizarContenido = async(req,res)=>{
@@ -42,31 +41,38 @@ exports.postActualizarContenido = async(req,res)=>{
     try{
         await Contenido.findOneAndUpdate({tipo:req.body.tipo,titulo:req.body.titulo},{año:req.body.año})
         console.log("Cambio realizado")
-        res.json({operacion: "correcta"})
+        res.status(200).json({operacion: "correcta"})
     }catch(err){
         console.log(err)
-        res.json({operacion: "incorrecta"})
+        res.status(422).json({operacion: "incorrecta"})
     }
 }
 
 exports.postBorrarContenido = async (req,res)=>{
     await Contenido.findOneAndDelete({tipo:req.body.tipo,titulo:req.body.titulo})
     console.log("Ha sido eliminadx")
-    res.json({operacion: "correcta"})
+    res.status(200).json({operacion: "correcta"})
 }
 
 //playlist
 exports.postAgregaraLaPlaylist = async (req,res)=>{
     try {
-        await Contenido.findOneAndUpdate({tipo:req.body.tipo,titulo:req.body.titulo},{playlist:req.body.playlist})
-        res.json({
-            "estado":"correcto"
-        })
-        
+        const dato = await Contenido.findOneAndUpdate({tipo:req.body.tipo,titulo:req.body.titulo},{playlist:req.body.playlist})
+        console.log(dato)
+        if(dato===null){
+            res.status(422).json({
+                "estado":"Contenido inexistente"
+            })
+        }
+        else{
+            res.status(200).json({
+                "estado":"correcto"
+            })
+        }
         
     } catch (error) {
         console.log(error)
-        res.json({
+        res.status(422).json({
             "estado":"incorrecto"
         })
     }
@@ -76,11 +82,11 @@ exports.postAgregaraLaPlaylist = async (req,res)=>{
 exports.getPlaylist = async (req,res)=>{
     try {
         const playlist = await Contenido.find({playlist:req.params.playlist})
-        res.json(playlist )
+        res.status(200).json(playlist )
         
     } catch (error) {
         console.log(error)
-        res.json({
+        res.status(422).json({
             "estado":"incorrecto"
         })
     }
@@ -91,10 +97,10 @@ exports.postActualizarPlaylist = async (req,res)=>{
     try{
         await Contenido.findOneAndUpdate({"playlist":req.body.playlist,"titulo":req.body.titulo},{"playlist":"none"})
         console.log("Cambio realizado")
-        res.json({operacion: "correcta"})
+        res.status(200).json({operacion: "correcta"})
     }catch(err){
         console.log(err)
-        res.json({operacion: "incorrecta"})
+        res.status(422).json({operacion: "incorrecta"})
     }
 
 }
@@ -104,10 +110,10 @@ exports.postBorrarPlaylist = async (req,res)=>{
         await Contenido.updateMany({"playlist":req.body.playlist},{"$set":{"playlist":"none"}})
         //console.log("Ha sido eliminadx")
         //await Contenido.remove()
-        res.json({operacion: "correcta"})
+        res.status(200).json({operacion: "correcta"})
         
     } catch (error) {
-        res.json({
+        res.status(422).json({
             "estado":"incorrecto"
         })
         
